c9993123aa768440600f5bb72d99338a160819f3
Badar <badar@devs-MacBook-Pro.local>
2020-11-27 23:45:05 -0500

    new pages added

--- src/components/Layout/Header-View.vue
+++ src/components/Layout/Header-View.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="mainHeader">
       <div class="headerview">
-          <a href="/"><img class="logo" src="@/assets/img/logo.jpg" alt="logo"></a>
+          <a href="/"><img class="logo" src="@/assets/img/logo.png" alt="logo"></a>
           <div class="navi">
-            <nav>
+            <nav class="desktop">
                   <ul>
                     <li><a href="/">Home</a></li>
+                    <li><a href="/#/about">About Us</a></li>
+                    <li><a href="/#/dental">Dental</a></li>
+                    <li><a href="/#/hydrate">Hydrate</a></li>
+                    <li><a href="/#/medical">Medical</a></li>
+                    <li><a href="/#/multivitamin">Multivitamin</a></li>
+                    <li><a href="/#/fooditems">Food Items</a></li>
+                    <li><a href="/#/footwear">Footwear</a></li>
+                    <li><a href="/#/optics">Optics</a></li>
+                    <li><a href="/#/watches">Watches</a></li>
+                    <li><a href="/#/contact-us">Contact Us</a></li>
+                  </ul>
+            </nav>
+            <nav class="mobile">
+                <ul class="mobile">
+                    <li><a href="/">Home mobile</a></li>
                     <li><a href="/#/about">About Us</a></li>
                     <li><a href="/#/dental">Dental</a></li>
                     <li><a href="/#/hydrate">Hydrate</a></li>
@@ -72,4 +87,23 @@ a:hover {
     justify-content: center;
     max-width: 300px;
 }
+
+.mobile {
+    display: none;
+}
+
+@media (max-width: 768px) {
+    .desktop {
+        display: none;
+    }
+    .mobile {
+        display: flex;
+        flex-direction: column;
+    }
+    .mobile li{
+        display: flex;
+        flex-direction: row;
+    }
+}
+
 </style>
